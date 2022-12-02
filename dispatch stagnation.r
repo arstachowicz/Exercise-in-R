0bef53e8d04754fadeeec1862db084e52fd76a81
Amanda Stachowicz <104032607+arstachowicz@users.noreply.github.com>
2022-12-02 12:51:10 -0600

    Combined chunks

--- dispatch stagnation.r
+++ dispatch stagnation.r
@@ -17,11 +17,11 @@ if (str_sub(raw_csv, -4, -1) == ".csv") {
 } else {
     print("Not a CSV file.")
 }
-```
 #clean up columns
 #remove spaces and hash in header
 colnames(dispatch_item_list) = gsub(" ", "_", colnames(dispatch_item_list))
 colnames(dispatch_item_list) = gsub("#", "", colnames(dispatch_item_list))
+```
 ```{r}
 #select necessary columns
 clean_dispatch <- dispatch_item_list %>%
@@ -32,19 +32,17 @@ cr_items <- clean_dispatch %>%
     filter(Current_Work_Center == "Cleanroom") %>%
 #change chr to date format in new column
     mutate(op_comp_date = mdy(Operation_Completion_Date)) %>%
-    select(-Operation_Completion_Date)
-```
-```{r}
 #Subtract system date from last operation date
 #clean up duplicates & filter by over 30 days old
-cr_items <- cr_items %>%
     mutate(wait_time = difftime(Sys.Date(), op_comp_date, units = "days")) %>%
     mutate(wait_time = as.numeric(wait_time)) %>%
     filter(wait_time > 30 & Job_Status == "Released") %>%
     distinct()
+colnames(cr_items)
 ```
 ```{r}
 grouped_by_part <- cr_items %>%
+    subset(!(Assembly == "03-10-0265" | Assembly == "03-10-0266")) %>%
     group_by(Assembly, .add = TRUE) %>%
     summarise(avg_wait = round(mean(wait_time), digits = 2),
         sum_quantity = sum(Quantity_Remaining)) %>%
@@ -57,21 +55,15 @@ grouped_by_wc <- cr_items %>%
     arrange(-sum_group)
 ```
 ```{r}
-#drop rows where certain part #'s are listed
-dia_removed <- grouped_by_part %>%
-    subset(!(Assembly == "03-10-0265" | Assembly == "03-10-0266"))
-```
-```{r}
 #Export df
 #Generate file name, paste0 removes space that occurs before periods
 file_path <- paste0("C:\\Users\\astachowicz\\Documents\\Weldment Tracker\\R Coded\\Dispatch List CR ", format(Sys.Date(),"%b%d%y"), ".csv") # nolint
-write.csv(dia_removed, file_path)
-```
-```{r}
+write.csv(grouped_by_part, file_path)
+
 #build graph matrices
-df10wait <- dia_removed %>%
+df10wait <- grouped_by_part %>%
     slice(1:10)
-df10qty <- dia_removed %>%
+df10qty <- grouped_by_part %>%
     arrange(-sum_quantity) %>%
     slice(1:10)
 
@@ -88,7 +80,8 @@ viz_10waits <- ggplot(data = df10wait,
         subtitle = "Stagnant Jobs in CR over 30 Days Old",
         x = "", y = "# of Days") +
     #removes gap between axis & bars
-    scale_y_continuous(expand = expansion(mult = c(0, .1)))
+    scale_y_continuous(expand = expansion(mult = c(0, .1))) +
+    coord_cartesian(ylim = c(30, max(df10wait$avg_wait) * 1.05))
 ggsave(paste0(format(Sys.Date(), "%b%d%y"), " top 10 waits.png"))
 
 #Top 10 Largest Quantities
@@ -123,3 +116,4 @@ viz_wc <- ggplot(data = grouped_by_wc,
     scale_y_continuous(expand = expansion(mult = c(0, .1)))
 ggsave(paste0(format(Sys.Date(), "%b%d%y"), " wc qty.png"))
 ```
+
